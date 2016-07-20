c2855ec0c709c735ab3f643fecd6d4af5dc26834
Mateen K <mateen.kadwaikar@provintl.com>
2016-07-20 13:09:14 +0530

    Removed Unwanted files and code refactoring

--- app/post-form.component.ts
+++ app/post-form.component.ts
@@ -1,30 +1,39 @@
-import { Component , Input} from '@angular/core';
-import { NgForm } from '@angular/common';
- 
-import { PostInterface } from './post-interface';
-import { PostService } from './post-service';
+import {
+  Component,
+  Input
+} from '@angular/core';
+import {
+  NgForm
+} from '@angular/common';
+
+import {
+  PostInterface
+} from './post-interface';
+import {
+  PostService
+} from './post-service';
 
 @Component({
-	selector: 'my-post-details',
-	templateUrl: 'app/post-form.html'
+  selector: 'my-post-details',
+  templateUrl: 'app/post-form.html'
 })
 
 export class PostDetailComponent {
-  
+
   @Input() selectedValue: PostInterface;
-  
-  constructor(private _demoService: PostService) { }
-  
-  submittedSuccessful : boolean = false;
-  
-  onSubmit(){
+
+  constructor(private _demoService: PostService) {}
+
+  submittedSuccessful: boolean = false;
+
+  onSubmit() {
     this._demoService.postJSON(this.selectedValue).subscribe(
-          data => {
-            console.log(data);
-            this.submittedSuccessful = true;
-           },
-            err => console.log(err),
-           () => console.log('Submitted successfully !')
-        )
+      data => {
+        console.log(data);
+        this.submittedSuccessful = true;
+      },
+      err => console.log(err),
+      () => console.log('Submitted successfully !')
+    )
   }
 }
